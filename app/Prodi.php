af4b3c215e1045b59b72090c1361733ea678ed6c
hasyirohmaelpan <hasyirohmaelpan@gmail.com>
2020-08-13 19:24:14 +0700

    UAS Fitur Prodi

--- app/Prodi.php
+++ app/Prodi.php
@@ -9,6 +9,10 @@ class Prodi extends Model
     protected $table = 'prodi';
     protected $primaryKey = 'kode_prodi';
 
+    protected $fillable = ['kode_prodi','nama_prodi','kaprodi',''];
+
+    protected $guarded = [];
+
     public function mahasiswa()
     {
         return $this->belongsTo('App\Mahasiswa','prodi','kode_prodi');
